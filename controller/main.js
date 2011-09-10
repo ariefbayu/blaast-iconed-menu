8ae8f0dfdf5b9fdd70b082fedcc5ae5614ff20fe
Arief Bayu Purwanto <ariefbayu@gmail.com>
2011-09-10 08:23:15 +0700

    fix overflow issue when child item is more than screen could handle

--- controller/main.js
+++ controller/main.js
@@ -10,72 +10,16 @@ _.extend(exports, {
     ':load': function() {
         var self = this;
         self.clear();
+        
         var i = 1;
         db.forEach(function(item){
             var temp;
             console.log('doing item #' + i);
-/*            if (i % 2 === 0) {
-                temp = new TextView({
-                    label: item.title,
-                    style: {
-                        color: 'black',
-                        width: 'fill-parent',
-                        'background-color': 'transparent'
-                    }
-                });
-                temp.on('blur', function(){
-                    this.style({
-                        'color': 'black',
-                        'background-color': 'transparent',
-                        'font-weight': 'normal'
-                    });
-                });
-            } else {
-                temp = new TextView({
-                    label: item.title,
-                    style: {
-                        color: 'black',
-                        width: 'fill-parent',
-                        'background-color': '#009eff'
-                    }
-                });
-                temp.on('blur', function(){
-                    this.style({
-                        'color': 'black',
-                        'background-color': '#009eff',
-                        'font-weight': 'normal'
-                    });
-                });
-            }
-            
-            temp = new SceneView({
-				style: {
-					width: 'fill-parent', 
-					height: imageHeight
-				}
-			});
-            
-			
-            temp.on('activate', function(){
-                app.setContent('list', {
-                    url: item.url,
-                    title: item.title
-                });
-            });
-            temp.on('focus', function(){
-                this.style({
-                    'color': 'black',
-                    'background-color': '#3682b0',
-                    'font-weight': 'bold'
-                });
-            });
-			
-            self.add(item.url, temp);*/
             self.add( item.url, misc.IconedMenu( item.title, 44, item.image, 40, 40, 'logo.png', 'header_background.png' ) );
 			
             i++;
         });
-        self.focusItem(1);
+        //self.get('LL').focusItem(1);
         console.log('View was loaded');
     },
 
